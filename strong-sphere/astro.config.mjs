c915c4b21a4978448ed7cacce4ec6ab22d4c6852
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2024-12-22 07:50:00 +0000

    Intergrate clerk for login

--- strong-sphere/astro.config.mjs
+++ strong-sphere/astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import clerk from "@clerk/astro";
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  integrations: [clerk()],
+  adapter: vercel(),
+  output: "server",
 });
